226e0f8de197b45dea25448d88f9ea0106a30dbd
MARTIN Boris <bmartin@softwaymedical.fr>
2026-05-15 14:07:57 +0200

    player: switch entry point to GeneticAgent + verbose MSG

--- src/main/java/com/bmrt/cgspring2026/Player.java
+++ src/main/java/com/bmrt/cgspring2026/Player.java
@@ -1,7 +1,7 @@
 package com.bmrt.cgspring2026;
 
 import com.bmrt.cgspring2026.action.Action;
-import com.bmrt.cgspring2026.greedy.GreedyAgent;
+import com.bmrt.cgspring2026.ga.GeneticAgent;
 import com.bmrt.cgspring2026.greedy.ShackAdjacency;
 import com.bmrt.cgspring2026.model.GameState;
 import com.bmrt.cgspring2026.pathfinding.PathTable;
@@ -16,25 +16,29 @@ public class Player {
         PathTable.init();
         ShackAdjacency.init();
 
-        GameState state = new GameState();
-        int[] actionBuf = new int[GameState.MAX_TROLLS + 1];
-        StringBuilder sb = new StringBuilder();
+        GameState state    = new GameState();
+        GeneticAgent agent = new GeneticAgent();
+        int[] actionBuf    = new int[GameState.MAX_TROLLS + 1];
+        StringBuilder sb   = new StringBuilder();
 
+        boolean firstTurn = true;
         while (true) {
             long start = System.nanoTime();
             state.readTurn(in);
 
-            int n = GreedyAgent.decide(state, actionBuf);
+            long deadline = start + (firstTurn ? GeneticAgent.INIT_BUDGET_NS
+                                               : GeneticAgent.TURN_BUDGET_NS);
+            int n = agent.decide(state, deadline, actionBuf);
+            firstTurn = false;
+
             sb.setLength(0);
             for (int i = 0; i < n; i++) {
                 if (i > 0) sb.append(';');
                 sb.append(Action.toCommand(actionBuf[i], state));
             }
-            sb.append(";MSG turn=")
-                    .append(state.turn)
-                    .append(" elapsed=")
-                    .append((System.nanoTime() - start) / 1_000_000)
-                    .append("ms");
+            sb.append(";MSG GA gen=").append(agent.lastGenCount())
+              .append(" fit=").append((int) agent.lastBestFitness())
+              .append(" t=").append((System.nanoTime() - start) / 1_000_000).append("ms");
             System.out.println(sb);
 
             state.turn++;
